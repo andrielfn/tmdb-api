e2f65aa1d3093c2a24cc0d65facde80cd6d0fd37
Andriel Nuernberg <andrielfn@gmail.com>
2013-05-06 22:00:56 -0300

    Get the release date by country for a specific movie id.

--- spec/tmdb/movie_spec.rb
+++ spec/tmdb/movie_spec.rb
@@ -425,7 +425,50 @@ describe TMDb::Movie do
           "name" => "gang war"
         }
       ])
+    end
+  end
 
+  describe '#releases' do
+    it 'returns the release date by country for a specific movie id' do
+      stub_get('/movie/598').to_return(json_response('find_movie_by_id.json'))
+      stub_get('/movie/598/releases').to_return(json_response('movie_releases.json'))
+
+      releases = TMDb::Movie.find(598).releases
+
+      expect(releases).to have(6).items
+
+      expect(releases).to match_array([
+        {
+          "iso_3166_1" => "RU",
+          "certification" => "",
+          "release_date" => "2002-02-05"
+        },
+        {
+          "iso_3166_1" => "US",
+          "certification" => "R",
+          "release_date" => "2002-08-31"
+        },
+        {
+          "iso_3166_1" => "BR",
+          "certification" => "",
+          "release_date" => "2002-08-30"
+        },
+        {
+          "iso_3166_1" => "CA",
+          "certification" => "",
+          "release_date" => "2002-09-06"
+        },
+        {
+          "iso_3166_1" => "GB",
+          "certification" => "18",
+          "release_date" => "2002-11-08"
+        },
+        {
+          "iso_3166_1" => "DE",
+          "certification" => "16",
+          "release_date" => "2003-05-08"
+        }
+      ])
     end
   end
 end
