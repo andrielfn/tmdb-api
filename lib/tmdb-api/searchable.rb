c29e8ce666a6ead6976eba59f950a3ba8fb25dfe
Andriel Nuernberg <andriel.nuernberg@plataformatec.com.br>
2013-08-04 00:51:35 -0300

    Remove unecessary comment

--- lib/tmdb-api/searchable.rb
+++ lib/tmdb-api/searchable.rb
@@ -19,7 +19,6 @@ module TMDb
     #
     # TMDb::Movie.search('Forret Gump', language: 'pt', year: 1994)
     def search(query, options = {})
-      # require "pry"; binding.pry
       options.merge!(query: query)
       result = Fetcher::get("/search/#{resource}", options)
       result['results'].map { |attributes| new(attributes) }
