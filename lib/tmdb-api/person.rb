6b44b78ddcfd2b439b4feb9b33cd32fb9b34d73b
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2013-08-08 22:39:06 -0300

    Add comments on find and images methods on Person.

--- lib/tmdb-api/person.rb
+++ lib/tmdb-api/person.rb
@@ -8,13 +8,29 @@ module TMDb
 
     attr_reader *ATTRIBUTES
 
+    # Public: Get the basic person information for a specific person ID.
+    #
+    # id      - The ID of the person.
+    # options - The hash options used to filter the search (default: {}):
+    #           More information about the options, check the api documentation
+    #
+    # Examples
+    #
+    # TMDb::Person.find(138)
     def self.find(id, options = {})
       res = get("/person/#{id}", query: options)
       res.success? ? Person.new(res) : bad_response(res)
     end
 
-    def self.images(id, options = {})
-      res = get("/person/#{id}/images", query: options)
+    # Public: Get the images for a specific person ID.
+    #
+    # id  - The person's ID
+    #
+    # Examples
+    #
+    # TMDb::Person.images(138)
+    def self.images(id)
+      res = get("/person/#{id}/images")
       res.success? ? res : bad_response(res)
     end
   end
