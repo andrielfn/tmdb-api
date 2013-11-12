5f1863c7c943a000813c71aa94589b23c7c2b1f2
Andriel Nuernberg <andrielfn@gmail.com>
2013-11-12 18:25:29 -0200

    Add support to set the API key from the environment variable

--- lib/tmdb-api.rb
+++ lib/tmdb-api.rb
@@ -19,8 +19,13 @@ require 'tmdb-api/version'
 
 module TMDb
   class << self
-    # Set the API Key that will be use to fetch the data.
-    attr_accessor :api_key
+    # Set the API Key that will be use to fetch the data or
+    # retrieve it from the environment variable $TMDB_API_KEY.
+    attr_writer :api_key
+
+    def api_key
+      @api_key || ENV['TMDB_API_KEY']
+    end
 
     # Set the default language of the fetched data.
     attr_accessor :default_language
--- spec/tmdb-api/tmdb_spec.rb
+++ spec/tmdb-api/tmdb_spec.rb
@@ -1,13 +1,19 @@
 require "spec_helper"
 
 describe TMDb do
-  it "sets a API Key" do
+  it "sets an API Key" do
     TMDb.api_key = '74e5e63643c787867565659583634766'
     expect(TMDb.api_key).to eql('74e5e63643c787867565659583634766')
   end
 
-  it 'raises when use no API' do
+  it 'raises when no API Key is set' do
     TMDb.api_key = ''
     expect { TMDb::Movie.find(550) }.to raise_error
+  end
+
+  it 'accepts the API Key from the environment variable' do
+    TMDb.remove_instance_variable(:@api_key)
+    ENV['TMDB_API_KEY'] = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33'
+    expect(TMDb.api_key).to eql('0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33')
   end
 end
