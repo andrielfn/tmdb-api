ffe87a128de9a72c1d17bf2f1a4b4657682eb418
Andriel Nuernberg <andrielfn@gmail.com>
2014-03-22 22:21:19 -0300

    Fix documentation

--- lib/tmdb-api/changes.rb
+++ lib/tmdb-api/changes.rb
@@ -5,14 +5,14 @@ module TMDb
     # of days that can be returned in a single request is 14.
     #
     # options - The hash options used to filter the search (default: {}).
-    #           :page - Page.
+    #           :page - Page. Minimum 1, maximum 1000.
     #           :start_date - Start date (YYYY-MM-DD).
     #           :end_date - End date (YYYY-MM-DD).
     #
     # Examples
     #
-    # TMDb::Movie.find(68721)
-    # TMDb::Movie.find(68721, language: 'pt')
+    #   TMDb::Changes.movies
+    #   TMDb::Changes.movies(page: 1, start_date: '2013-03-22')
     def self.movies(options = {})
       res = get('/movie/changes', query: options)
       res.success? ? res : bad_response(res)
